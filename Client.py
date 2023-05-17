18afc0b3ee19f3a57164597e714a1a3887a67124
sayedmuhammadyasin-usman <sayedmuhammadyasin.usman@vodafone.com>
2023-05-17 16:22:10 +0100

    refactor & formatting: added comments and changed naming conventions all to snake_case

--- Client.py
+++ Client.py
@@ -3,74 +3,83 @@
 #Olivia McAndrew & Yasin Usman
 
 #Modules 
-import socket
-import json
-import re  # Regular Expression module for input validation
-from prettytable import PrettyTable
+import socket #Imported for creating the socket for client-server functionality
+import json #Module to make handling JSON easy.
+import re  #REGEX module for input validation to create parameters
+from prettytable import PrettyTable #table module to nicely present our JSON data
 
 class Person:                   
     def __init__(self):         # Class for inputted user.
-        # Input validation for first name: allows only letters and it should not be empty.
+        # Input validation for first name using REGEX: allows only letters and it should not be empty or have any spaces.
         while True:
-            self.fname = input("Your first name: ")
-            if re.match("^[A-Za-z]*$", self.fname) and self.fname:
+            self.first_name = input("Your first name: ")
+            if re.match("^[A-Za-z]*$", self.first_name) and self.first_name:
                 break
             else:
                 print("Invalid input. Please enter a valid first name.")
 
-        # Input validation for last name: allows only letters and it should not be empty.
+        # Input validation for last name using REGEX: allows only letters and it should not be empty or have any spaces.
         while True:
-            self.sname = input("Your last name: ")
-            if re.match("^[A-Za-z]*$", self.sname) and self.sname:
+            self.surname = input("Your last name: ")
+            if re.match("^[A-Za-z]*$", self.surname) and self.surname:
                 break
             else:
                 print("Invalid input. Please enter a valid last name.")
 
-        # Input validation for age: allows only numbers and age should be between 1 to 120.
+        # Input validation for age using REGEX: allows only numbers and age should be between 1 to 120.
         while True:
             self.age = input("Your Age: ")
-            if re.match("^[1-9][0-9]?$|^120$", self.age):
+            if re.match("^[1-9][0-9]?$|^100$", self.age):
                 break
             else:
-                print("Invalid input. Please enter a valid age between 1 to 120.")
+                print("Invalid input. Please enter a valid age between 1 to 100.")
 
-        # Input validation for employment status: allows only 'y' or 'n'.
+        # Input validation for employment status using REGEX: allows only 'y' or 'n'.
         while True:
-            self.eRecord = input("Your employment status, y or n: ")
-            if re.match("^[ynYN]$", self.eRecord):
+            self.employment_record = input("Your employment status, y or n: ")
+            if re.match("^[ynYN]$", self.employment_record):
                 break
             else:
                 print("Invalid input. Please enter 'y' for yes or 'n' for no.")
 
-    def displayPerson(self):    # Display user inputs.
-        print("\nFirst name:", self.fname, "\nLast name:", self.sname, "\nAge:", self.age, "\nEmployment Status", self.eRecord)
+    # Display user inputs.
+    def displayPerson(self):
+        print("\nFirst name:", self.first_name, "\nLast name:", self.surname, "\nAge:", self.age, "\nEmployment Status", self.employment_record)
 
-    def to_dict(self):   # Return dictionary representation of person
+    # Return dictionary representation of person similar to JSON
+    def to_dict(self):   
         return {
-            'First name': self.fname,
-            'Last name': self.sname,
+            'First name': self.first_name,
+            'Last name': self.surname,
             'Age': self.age,
-            'Employment Status': self.eRecord
+            'Employment Status': self.employment_record
         }
 
-#Variables&Constants 
 
 #Code
 def Main():
+    #Same host port and socket code as server to create that connection
     host = '127.0.0.1'
     port = 8989
     s = socket.socket()
     
-    try:  # Try to connect to the server
+    # Conenction to server
+    try: 
         s.connect((host,port))
         print("\nConnected to the server successfully.")
-    except socket.error as ERROR:  # If the server is not connected, it shows an error message and ends the program
+    # If the server is not connected, it shows an error message and ends the program
+    except socket.error as ERROR:  
         print("Error occurred while connecting to the server: ", ERROR)
         return
 
+# ****************************************************************  
+
+        # USER WELCOME MENU
+
+# ****************************************************************  
+ 
     while True:
         print("\nWelcome to Employment record centre!!!")
-        # Input validation for the main menu: only allows 1, 2 or 3.
         while True:
             menu = input(""" 
             Please select an Option:
@@ -78,73 +87,86 @@ def Main():
             2 = Add data to employment records
             3 = Exit
             """)
-            # Here we use the regex pattern to check if the input is valid (1, 2, or 3)
+            # Input validation for the main menu so it only allows 1, 2 or 3 as an input, specifed using REGEX
             if re.match("^[123]$", menu):
                 menu = int(menu)  # Convert the validated input to integer
                 break
             else:
                 print("Invalid input. Please enter 1, 2 or 3.")
 
+# ****************************************************************************  
 
-        #USER IS DISPLAYED WITH DATA
+        # USER OPTION 1 - USER CAN DISPLAY AND SEARCH WITHIN JSON DATA
+
+# ****************************************************************************    
         if menu == 1:
-            # Display employment records
+            #Displays employment records json data.
             try:
-                # Making the path to the file OS agnostic
+                #Making the path to the file OS agnostic
                 with open('EmploymentRecords.json', 'r') as f:
                     records = json.load(f)
             except (FileNotFoundError, json.JSONDecodeError) as e:
-                # Detailed error message here for no records
+                #Sends error message if no records or JSON are found
                 print(f"Error occurred while trying to read records: {str(e)}")
                 print("No records found.")
                 continue
 
-            # Display data in a table format
+            #Displays JSON data in a prettytable format
+            #Creates table using the inputs as column titles.
             table = PrettyTable(['First Name', 'Last Name', 'Age', 'Employment Status'])
+            #For loop to add each found result in a table from the JSON file
             for record in records:
                 table.add_row([record['First name'], record['Last name'], record['Age'], record['Employment Status']])
             print(table)
 
-            # Add an option to search for a person after displaying records
+            #Search functionality to search name within Records, loop to keep search going until exit, which sends them back to the menu
             while True:
-                display_records(records)
-                choice = input("Type 'search' to search for a person, or type 'exit' to return to the menu: ")
-                if choice.lower() == "search":
-                    search_person(records)
-                elif choice.lower() == "exit":
+                #Shows a the pretty table of the JSON data
+                table = PrettyTable(['First Name', 'Last Name', 'Age', 'Employment Status'])
+                for record in records:
+                    table.add_row([record['First name'], record['Last name'], record['Age'], record['Employment Status']])
+                print(table)
+                #Prompts the user to type search to begin using the search function or exit back to the menu
+                search_choice = input("Type 'search' to search for a person, or type 'exit' to return to the menu: ")
+                if search_choice.lower() == "search":
+                    search_person_within_records(records)
+                elif search_choice.lower() == "exit":
                     break
-        # ****************************************************************
+# ****************************************************************  
 
-        #USER INPUTS DATA
+        # USER OPTION 2 - USER INPUTS DATA
+
+# ****************************************************************  
         elif menu == 2:
             while True:
+                #Uses the Person Class made above, to specify user input, and put it in a dict form similar to JSON
                 print("To enter your employment records please enter the information")  
                 person = Person()
                 person_dict = person.to_dict()
 
-                # Display the new record in a pretty table for confirmation
+                #Displays the new record in a pretty table for confirmation before adding to the JSON file
                 table = PrettyTable(['First Name', 'Last Name', 'Age', 'Employment Status'])
                 table.add_row([person_dict['First name'], person_dict['Last name'], person_dict['Age'], person_dict['Employment Status']])
                 print("\nHere is your new record:")
                 print(table)
 
+                #Prompts user to check if the record is correct, if it is it adds to JSON file, if not, it is deleted and user is prompted to add it again, this starts the while loop again.
                 confirm = input("Is this correct? (y/n): ")
                 if confirm.lower() not in ['y', 'yes']:
                     print("Please add the record again.")
                     continue
                 
-                # save data to json file
+                #Reads JSON data, and raises error if no records are found
                 try:
-                    # Making the path to the file OS agnostic
                     with open('EmploymentRecords.json', 'r') as f:
                         people = json.load(f)
                 except (FileNotFoundError, json.JSONDecodeError) as e:
                     print(f"Error occurred while trying to read records: {str(e)}")
                     people = []
+                
 
+                #Writes the newly inputted data into the JSON in dictonary format within the array.
                 people.append(person.to_dict())
-
-                # Try/except block for file write operation
                 try:
                     with open('EmploymentRecords.json', 'w') as f:
                         json.dump(people, f)
@@ -152,64 +174,55 @@ def Main():
                 except Exception as e:
                     print(f"Error occurred while trying to write to file: {str(e)}")
 
-                continue_ = input("Would you like to add another status: y or n ") 
+                #Prompts the user if they would like to add another emplloyee record, to the JSON.
+                continue_ = input("Would you like to add another employee record: y or n ") 
                 if continue_.upper() == "N":
                     break
-        # ****************************************************************  
 
-        #USER EXITS PAGE
+# ****************************************************************  
+
+        # USER OPTION 3 - EXITS PAGE
+
+# ****************************************************************  
         elif menu ==  3: 
             print("NOT ready sorry")
             break
-
         else:
             print("Invalid option selected, please try again.")
-
+    #Closes socket
     s.close() 
 
-def search_person(records):
+# Function used in menu option 1 to search person within records
+def search_person_within_records(records):
     while True:
+        # Prompts the user to search using the exact first name of the person
         search_term = input("Enter the first name of the person to search for or 'exit' to return to display all records: ")
+        #Checks if user has entered exit to break the loop, however if first name begins with exit it breaks.
         if search_term.lower() == "exit":
             break
         found_records = [record for record in records if record['First name'].lower() == search_term.lower()]
-    
+        # Checks if any of the same records of first name are found, if yes, displays pretty table of matching results, similar to main display table.
         if found_records:
+            #Creates table using the inputs as column titles.
             table = PrettyTable(['First Name', 'Last Name', 'Age', 'Employment Status'])
+            #For loop to add each found result in a table from the JSON file
             for record in found_records:
                 table.add_row([record['First name'], record['Last name'], record['Age'], record['Employment Status']])
             print(table)
         else:
+            #If no result is found prints this message
             print("No person found with the given first name.")
             continue
-
-def display_records(records):
-    table = PrettyTable(['First Name', 'Last Name', 'Age', 'Employment Status'])
-    for record in records:
-        table.add_row([record['First name'], record['Last name'], record['Age'], record['Employment Status']])
-    print(table)
 
 if __name__ == '__main__':
     Main()
 
 
-    #Needs to be done 
-    #GUI
-
-    # '''
-    # s = socket.socket()
-    # s.connect((host,port))   #creates socket with server details
-
-    
-    # message = input("You are connected to the server. Enter Your Name:")
-    # while message != 'q':
-    #     s.send(message.encode('utf-8'))
-
-    #     data = s.recv(1024).decode('utf-8')
-    #     print("Message recevied from server: " + str(data))
-    #     message = input("-->")
-    # s.close() 
-    # '''
+#TO DO LIST
+#Refactoring and Commenting Code
+#Browser displayed content(dev)
+#Heterogeneous systems architectures (i.e., different O/S) (dev) 
+#GUI or an output of stats and data.
     
 
 
--- Server.py
+++ Server.py
@@ -1,48 +1,54 @@
 #04.04.2023
 #Employment Status - Server 
-#Olivia McAndrew & Yasin Usman
+#Yasin Usman & Olivia McAndrew
 
 
 #Modules 
-import socket
-import threading  # Imported for multi-threading
+import socket #Imported for creating the socket for client-server functionality
+import threading  #Imported for multi-threading
 
 #Code
-def handle_client(c):
+def send_messages_from_server(c):
+    #Loop that runs so messages can be sent from the server
     while True:
-        data = c.recv(1024).decode('utf-8')     # Receives data from the client
-        if not data: 
-            break       # Ends the connection if no data is received
-        
-        # If the client sent a specific message, output a message accordingly.
-        if data == "Data added to JSON":
+        #Pulls data from the client
+        response_data = c.recv(1024).decode('utf-8')
+        # If no response is given the connection ends.
+        if not response_data: 
+            break       
+        #If the response the server gets is "Data added to JSON" then a respective message gets sent
+        if response_data == "Data added to JSON":
             print("Data has been added to the JSON file!")
         else:
-            print("The connected user says: " + data)
-            data = data.upper()  # Converts the data to uppercase
-            print("Sending back in upper case!: " + data)
-            c.send(data.encode('utf-8'))    # Sends the uppercase data back to the client
+            print("The connected user says: " + response_data)
+            data = response_data.upper()  # Converts the data to uppercase
+            print("Sending back in upper case!: " + response_data)
+            c.send(response_data.encode('utf-8'))#sends the exact response to the client
+    #Closes the connection to the client
+    c.close()
 
-    c.close()  # Closes the connection to the client
+def main():
+    host = '127.0.0.1' #IP address that is custom created
+    port = 8989 #Port number for server to connect to
 
-def Main():
-    host = '127.0.0.1'
-    port = 8989
+    s = socket.socket() #Creates a new socket object
+    s.bind((host,port)) #Binds the socket to a specific address and port that we mentioned above
 
-    s = socket.socket()  # Creates a new socket object
-    s.bind((host,port))  # Binds the socket to a specific address and port
-
-    s.listen(1)  # Configures the socket to accept connections
+    s.listen(1) #listens and accepts the connection
+    #prints confirmation response to show the server is running
     print("Employment Status server is running...")
     print("Machine can connect on port 8989")
 
     while True:
-        c, addr = s.accept()     # Waits for a client to connect and then accepts the connection
+        # Waits for a client to connect and then accepts the connection
+        c, addr = s.accept()
         print("Connection from: " + str(addr))
 
-        # Start a new thread to handle the client
-        client_thread = threading.Thread(target=handle_client, args=(c,))
-        client_thread.start()
+        # Starts a new thread pulling the function send_messages_from_server to handle the client data and responses. A way to encompass Multi-threaded client/server functionality 
+        create_client_thread = threading.Thread(target=send_messages_from_server, args=(c,))
+        create_client_thread.start()
 
+
+#Executes main function
 if __name__ == '__main__':
-    Main()
+    main()
